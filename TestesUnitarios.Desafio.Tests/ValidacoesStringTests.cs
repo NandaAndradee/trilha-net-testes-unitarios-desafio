298c26e2a26524d8420ba25a3e478c62b2195026
Fernanda Andrade <nandafmna@hotmail.com>
2024-01-13 18:06:55 -0300

    Desafio concluído!

--- TestesUnitarios.Desafio.Tests/ValidacoesStringTests.cs
+++ TestesUnitarios.Desafio.Tests/ValidacoesStringTests.cs
@@ -24,12 +24,10 @@ public class ValidacoesStringTests
         var texto = "Esse é um texto qualquer";
         var textoProcurado = "qualquer";
 
-        //TODO: Corrigir a chamada do método "ContemCaractere" da seção Act
         // Act
          var resultado = _validacoes.ContemCaractere(texto, textoProcurado);
 
         // Assert
-        //TODO: Corrigir o Assert.True com base no retorno da chamada ao método
         Assert.True(resultado);
     }
 
@@ -44,16 +42,12 @@ public class ValidacoesStringTests
         var resultado = _validacoes.ContemCaractere(texto, textoProcurado);
 
         // Assert
-        //TODO: Corrigir o Assert.False com base no retorno da chamada ao método
         Assert.False(resultado);
     }
 
-    //TODO: Corrigir a anotação [Fact]
     [Fact]
     public void TextoDeveTerminarComAPalavraProcurado()
     {
-        //TODO: Corrigir a variável "textoProcurado" seção Arrange
-
         // Arrange
         var texto = "Começo, meio e fim do texto procurado";
         var textoProcurado = "procurado";
